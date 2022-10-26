2267211de3c12cbae16ea0d54d9d5d4284fa1cbd
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-26 15:32:28 +0200

    add-img

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,35 @@ const cities = [
     {
         name: 'New York',
         people: '8,468 milioni',
-        descr: 'Si trova alla foce del fiume Hudson. Manhattan, il suo cuore pulsante, è considerato uno dei poli commerciali più importanti al mondo.'
+        descr: 'Si trova alla foce del fiume Hudson. Manhattan, il suo cuore pulsante, è considerato uno dei poli commerciali più importanti al mondo.',
+        image: 'new-york.jpg'
     },
 
     {
         name: 'Parigi',
         people: '2,161 milioni',
-        descr: 'Capitale della Francia, è una delle città più importanti di Europa, centro mondiale di arte, moda, gastronomia e cultura.'
+        descr: 'Capitale della Francia, è una delle città più importanti di Europa, centro mondiale di arte, moda, gastronomia e cultura.',
+        image: 'parigi.jpg'
     },
 
     {
         name: 'Roma',
         people: '2,873 milioni',
-        descr: 'Capitale dell’Italia, è una grande città cosmopolita con una storia artistica, architettonica e culturale che ha influenzato tutto il mondo e che risale a quasi 3000 anni fa.'
+        descr: 'Capitale dell’Italia, è una grande città cosmopolita con una storia artistica, architettonica e culturale che ha influenzato tutto il mondo e che risale a quasi 3000 anni fa.',
+        image: 'roma.jpeg',
     },
 
     {
         name: 'Barcellona',
         people: '1,62 milioni',
-        descr: 'La cosmopolita capitale della regione spagnola della Catalogna, è celebre soprattutto per arte e architettura.'
+        descr: 'La cosmopolita capitale della regione spagnola della Catalogna, è celebre soprattutto per arte e architettura.',
+        image: 'barcellona.jpg'
     },
 
     {
         name: 'Berlino',
         people: '3,645 milioni',
-        descr: 'La capitale della Germania, fu fondata nel XIII secolo. Il Memoriale dell Olocausto e le parti restanti del muro di Berlino, testimoniano la difficile storia della città nel corso del XX secolo.'
+        descr: 'La capitale della Germania, fu fondata nel XIII secolo. Il Memoriale dell Olocausto e le parti restanti del muro di Berlino, testimoniano la difficile storia della città nel corso del XX secolo.',
+        image: 'berlino.jpg'
     }
 ]
