d7e25075bce1b292d8658a2f3bd8a77ed84334e0
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-26 15:26:17 +0200

    add-objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
 // js
+
+const cities = [
+    {
+        name: 'New York',
+        people: '8,468 milioni',
+        descr: 'Si trova alla foce del fiume Hudson. Manhattan, il suo cuore pulsante, è considerato uno dei poli commerciali più importanti al mondo.'
+    },
+
+    {
+        name: 'Parigi',
+        people: '2,161 milioni',
+        descr: 'Capitale della Francia, è una delle città più importanti di Europa, centro mondiale di arte, moda, gastronomia e cultura.'
+    },
+
+    {
+        name: 'Roma',
+        people: '2,873 milioni',
+        descr: 'Capitale dell’Italia, è una grande città cosmopolita con una storia artistica, architettonica e culturale che ha influenzato tutto il mondo e che risale a quasi 3000 anni fa.'
+    },
+
+    {
+        name: 'Barcellona',
+        people: '1,62 milioni',
+        descr: 'La cosmopolita capitale della regione spagnola della Catalogna, è celebre soprattutto per arte e architettura.'
+    },
+
+    {
+        name: 'Berlino',
+        people: '3,645 milioni',
+        descr: 'La capitale della Germania, fu fondata nel XIII secolo. Il Memoriale dell Olocausto e le parti restanti del muro di Berlino, testimoniano la difficile storia della città nel corso del XX secolo.'
+    }
+]
